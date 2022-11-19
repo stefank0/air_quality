9ce2e5735f05a78409881be231e6a1b9860c503b
stefank0 <stefankorenberg@hotmail.com>
2022-11-19 15:23:44 +0100

    commit old example.py

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+import csv
 import time
 from datetime import datetime
 
@@ -11,30 +12,41 @@ from sps30 import SPS30
 sps = SPS30()
 
 try:
-	sps.start_measurement()
-	with LinuxI2cTransceiver('/dev/i2c-1') as i2c_transceiver:
-		connection = I2cConnection(i2c_transceiver)
-		sgp40 = Sgp40I2cDevice(connection)
-		shtc3 = Shtc3I2cDevice(connection)
-		i = 0
-		while True:
-			i += 1
-			sleep_time = 1.0 - (time.time() % 1.0)		# wait remaining time of this second
-			if sleep_time < 0.5:
-				print(f'sleep_time: {sleep_time}')
-			time.sleep(sleep_time)
-			pm = sps.get_measurement()['sensor_data']
-			pm2_5_mass = pm['mass_density']['pm2.5']
-			pm2_5_count = pm['particle_count']['pm2.5']
-			pm10_mass = pm['mass_density']['pm10']
-			pm10_count = pm['particle_count']['pm10']
-			pm_size = pm['particle_size']
-			t, rh = shtc3.measure()
-			t, rh = t.degrees_celsius, rh.percent_rh
-			voc = sgp40.measure_raw(relative_humidity=rh, temperature=t).ticks
-			if i % 1 == 900:
-				now = datetime.now().strftime("%Y-%m-%d %H:%M")
-				print(now, round(t, 1), round(rh, 1), voc, round(pm2_5_mass, 1), round(pm2_5_count, 1), sep='\t')
+	with open('data.csv', 'a', newline='') as f:
+		writer = csv.writer(f)
+		sps.start_measurement()
+		with LinuxI2cTransceiver('/dev/i2c-1') as i2c_transceiver:
+			connection = I2cConnection(i2c_transceiver)
+			sgp40 = Sgp40I2cDevice(connection)
+			shtc3 = Shtc3I2cDevice(connection)
+			rows = []
+			i = 0
+			while True:
+				i += 1
+				sleep_time = 1.0 - (time.time() % 1.0)		# wait remaining time of this second
+				if sleep_time < 0.5:
+					print(f'sleep_time: {sleep_time}')
+				time.sleep(sleep_time)
+				pm = sps.get_measurement()['sensor_data']
+				pm2_5_mass = pm['mass_density']['pm2.5']
+				pm2_5_count = pm['particle_count']['pm2.5']
+				pm10_mass = pm['mass_density']['pm10']
+				pm10_count = pm['particle_count']['pm10']
+				pm_size = pm['particle_size']
+				t, rh = shtc3.measure()
+				t = round(t.degrees_celsius, 3)
+				rh = round(rh.percent_rh, 3)
+				voc = sgp40.measure_raw(relative_humidity=rh, temperature=t).ticks
+				if i % 60 == 0:
+					now = datetime.now().strftime("%Y-%m-%d %H:%M")
+					row = [now, t, rh, voc, pm2_5_mass, pm2_5_count, pm10_mass, pm10_count, pm_size]
+					rows.append(row)
+					if i % 600 == 0:
+						print(*row, sep='\t')
+				if len(rows) >= 120:
+					writer.writerows(rows)
+					f.flush()
+					rows.clear()
 except Exception as e:
 	print('Closing')
 	sps.close()
